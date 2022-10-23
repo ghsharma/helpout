358bf949cd6fca5889d397e30bcb8697f83d0e84
Govind Harsh Sharma <95496933+ghsharma@users.noreply.github.com>
2022-10-24 00:25:49 +0530

    initial commit

---
+++ merrygoround.c
@@ -0,0 +1,54 @@
+
+#include<stdio.h>
+int main(){
+
+    for(int i=1;i<=10;i++){
+
+    printf("10*%d = %d\n",i ,10*i);
+    }
+    return 0;
+
+}
+
+
+
+
+#include<stdio.h>
+int main(){
+
+    int i,sum=0;
+
+    for(i=1;i<=10;i++){
+        sum+=i;
+
+        printf("the sum of first 10 natural numbers is = %d\n",sum);
+    }
+        return 0;
+
+}
+
+#include<stdio.h>
+int main(){
+
+    int i , sum=0, n=8;
+
+    for(i=1;i<=10;i++){
+            printf("8 x %d = %d\n",i,8*i);
+    }
+            for(i=1;i<=n;i++){
+        sum *=i;
+
+            printf("the sum of the table counts %d\n",sum);
+            }
+    
+    
+    return 0;
+
+}
+
+
+#include<stdio.h>
+int main(){
+
+
+}
